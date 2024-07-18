d4d50ff52c9980305c8c0f7d856a1adb764aa6c6
aizatshah20 <aizatshah20@gmail.com>
2024-07-18 12:50:46 +0800

    upload sql file

---
+++ project_sql/03_top_demanded_skills..sql
@@ -0,0 +1,46 @@
+/*
+
+Question: What are the most in-demand skills for data analysts?
+- Identify the top 5 in-demand skills for a data analyst.
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market, 
+  providing insights into the most valuable skills for job seekers.
+
+- join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst
+- Focus on all job postings
+- WHy? Retrieves the top 5 skills with the highest demand on the job market,
+  providing insights into the most valuable skills for job seekers
+
+*/
+
+SELECT 
+  skills,
+  COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+  job_title_short = 'Data Analyst'
+GROUP BY
+  skills
+ORDER BY
+  demand_count DESC
+LIMIT 5
+
+-- specifying for only remote job
+
+SELECT 
+  skills,
+  COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+  job_title_short = 'Data Analyst' AND
+  job_work_from_home = True
+GROUP BY
+  skills
+ORDER BY
+  demand_count DESC
+LIMIT 5
